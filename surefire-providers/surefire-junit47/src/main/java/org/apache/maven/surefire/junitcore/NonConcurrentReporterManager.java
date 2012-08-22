63678ca1465d8ced5c150deed8d3fe21d83910b7
Nicolas Liochon <nkeywal@gmail.com>
2012-08-22 20:45:25 +0200

    q

--- surefire-providers/surefire-junit47/src/main/java/org/apache/maven/surefire/junitcore/NonConcurrentReporterManager.java
+++ surefire-providers/surefire-junit47/src/main/java/org/apache/maven/surefire/junitcore/NonConcurrentReporterManager.java
@@ -5,18 +5,29 @@ import org.apache.maven.surefire.report.ConsoleLogger;
 import org.apache.maven.surefire.report.ConsoleOutputReceiver;
 import org.apache.maven.surefire.report.ReportEntry;
 import org.apache.maven.surefire.report.ReporterFactory;
+import org.apache.maven.surefire.report.RunListener;
 import org.apache.maven.surefire.testset.TestSetFailedException;
 
 import java.util.Map;
 
 public class NonConcurrentReporterManager extends ClassesParallelRunListener {
 
+  @Override
   public synchronized void writeTestOutput( byte[] buf, int off, int len, boolean stdout )
   {
       ((ConsoleOutputReceiver)getRunListener()).writeTestOutput( buf, off, len, stdout );
   }
 
+
+  private RunListener runListener;
+  @Override
+  RunListener getRunListener()
+  {
+    return runListener;
+  }
+
   public NonConcurrentReporterManager(Map<String, TestSet> classMethodCounts, ReporterFactory reporterFactory, ConsoleLogger consoleLogger) throws TestSetFailedException {
     super( classMethodCounts, reporterFactory, consoleLogger );
+    runListener = reporterFactory.createReporter();
   }
 }
